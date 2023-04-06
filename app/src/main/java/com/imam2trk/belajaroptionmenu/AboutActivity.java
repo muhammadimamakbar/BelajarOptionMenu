fe2f4b91cf8b67e1fe8f555bcf70a5acbbf1615c
320-PC17 <imam21trk@mahasiswa.pcr.ac.id>
2023-04-06 08:11:51 +0700

    PCR-320

---
+++ app/src/main/java/com/imam2trk/belajaroptionmenu/AboutActivity.java
@@ -0,0 +1,13 @@
+package com.imam2trk.belajaroptionmenu;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
